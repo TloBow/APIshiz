0d13ec223773c6a587fb4eeecb20c26f9aef3de3
Tim Patschke <patschke.tim@it.ausbildung.de>
2022-11-01 12:51:30 +0100

    first commit

---
+++ APOD/main.py
@@ -0,0 +1,45 @@
+from random import Random
+import requests
+import json
+
+def PicOfTheDay():
+    r = requests.get("https://api.nasa.gov/planetary/apod?api_key=NIFH1JyVc1eMgvFxXShtJkGWgPAMFFjoCKJpcDmB")
+    print(r)
+    load = json.loads(r.content)
+
+    receive = requests.get(load["hdurl"])
+
+    picture = open('picoftheday.png', 'wb')
+    picture.write(receive.content)
+    picture.close()
+
+    explanation = open("picoftheday.txt", "w")
+    explanation.write("date: " + load['date'])
+    explanation.write("\nexplanation: " + load['explanation'])
+    explanation.close
+
+def RandomPic():
+    amount_str = input("Enter amount of pictures: ")
+    amount_int = int(amount_str)
+
+    r = requests.get("https://api.nasa.gov/planetary/apod?count=" + amount_str + "&api_key=NIFH1JyVc1eMgvFxXShtJkGWgPAMFFjoCKJpcDmB")
+    print(r)
+    load = json.loads(r.content)
+
+    for x in range(amount_int):
+        recpic = requests.get(load[x]['hdurl'])
+
+        number = str(x)
+
+        picture = open("randompic" + number + ".png", "wb")
+        picture.write(recpic.content)
+        picture.close()
+
+        explanation = open("randompic" + number + ".txt", "w")
+        explanation.write("date: " + load[x]['date'])
+        explanation.write("\nexplanation: " + load[x]['explanation'])
+        explanation.close()
+
+
+
+RandomPic()
